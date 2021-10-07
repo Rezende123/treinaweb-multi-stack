a1a6628ed7a70b2627fdda3fc5a95382073845b4
Rezende123 <feliperezende.srz@gmail.com>
2021-10-06 23:34:40 -0300

    feat: Card de informações

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import SafeEnvironment from '../ui/components/feedback/SafeEnvironment/SafeEnvironment'
 import PageTitle from '../ui/components/data-display/PageTitle/PageTitle'
+import UseInformation from '../ui/components/data-display/UseInformation/UseInformation'
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +10,37 @@ const Home: NextPage = () => {
       <PageTitle
         subtitle={"Aquele"}
         title={"Conheça os profissionais"} />
+
+      <UseInformation
+        name={'Felipe Rezende'}
+        picture={'https://github.com/Rezende123.png'}
+        rating={4}
+        description={'Salvador'}
+      />
+      <UseInformation
+        name={'Felipe Rezende'}
+        picture={'https://github.com/Rezende123.png'}
+        rating={4}
+        description={'Salvador'}
+      />
+      <UseInformation
+        name={'Felipe Rezende'}
+        picture={'https://github.com/Rezende123.png'}
+        rating={4}
+        description={'Salvador'}
+      />
+      <UseInformation
+        name={'Felipe Rezende'}
+        picture={'https://github.com/Rezende123.png'}
+        rating={4}
+        description={'Salvador'}
+      />
+      <UseInformation
+        name={'Felipe Rezende'}
+        picture={'https://github.com/Rezende123.png'}
+        rating={4}
+        description={'Salvador'}
+      />
     </div>
   )
 }
